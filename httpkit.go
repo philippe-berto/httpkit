aa016ca119553fd3b6ac28c763764fb9234bf160
Philippe Berto <philippe.berto@ispot.tv>
2025-07-02 16:19:22 -0300

    Update CORS headers to include User-Address and Token

--- httpkit.go
+++ httpkit.go
@@ -141,7 +141,7 @@ func cors(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Origin", CorsAllowOrigins)
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
-		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization, User-Address, Token")
 		w.Header().Set("Access-Control-Max-Age", "3600")
 
 		if r.Method == http.MethodOptions {
